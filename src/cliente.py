b6832b5cb42b0c2efd8b531fea6f14b4c92ad32a
pedronassero <pedronasserog@gmail.com>
2024-11-05 16:15:23 -0300

    feat: handling client register

--- src/cliente.py
+++ src/cliente.py
@@ -1,18 +1,55 @@
 from socket import *
+import time
+import os
+
+
+def initializing():
+    os.system('cls')
+    print("\033[33mInicializing\033[0m", end='')
+    
+    for _ in range(3):
+        print("\033[33m.\033[0m", end='', flush=True)
+        time.sleep(1)
+    
+    os.system('cls')
+
+
+def find_available_port():
+    temp_socket = socket(AF_INET, SOCK_STREAM)
+    temp_socket.bind(('', 0))
+    available_port = temp_socket.getsockname()[1]
+    temp_socket.close()
+
+    return available_port
 
 
 """UDP connection with Server"""
-server = 'localhost'
-server_port = 13377
-udp_client = socket(AF_INET, SOCK_DGRAM)
+def udp_server():
+    server = 'localhost'
+    server_port = 13377
+    udp_client = socket(AF_INET, SOCK_DGRAM)
 
-message = input("Message: ").encode()
-udp_client.sendto(message, (server, server_port))
+    message = input("\033[36mMessage: \033[0m").encode()
+    udp_client.sendto(message, (server, server_port))
 
-try:
-    response, server_address = udp_client.recvfrom(2048)
-    print("Response:", response.decode())
-except ConnectionResetError as e:
-    print("Connection was reset by the server:", e)
+    try:
+        response, server_address = udp_client.recvfrom(2048)
+        print("\033[36mResponse:\033[0m", response.decode())
+    except ConnectionResetError as e:
+        print("Connection was reset by the server:", e)
 
-udp_client.close()
+    udp_client.close()
+
+
+def main():
+    initializing()
+    print("\033[34mBefore connecting to the server, take note of your available port:\033[0m", end='')
+    print(f"\033[32m {find_available_port()}\033[0m")
+    udp_server()
+
+
+"""TCP connection with Peer"""
+
+
+if __name__ == '__main__':
+    main()
--- src/servidor.py
+++ src/servidor.py
@@ -1,17 +1,59 @@
 from socket import *
 import threading
+import hashlib
 
 online_clients = {}
-shared_images = {}
+available_images = {}
+
+def handle_register(message, client_address, server):
+    """
+    Recieves a PASSWORD chosen by the client,
+    a PORT NUMBER chosen for TCP connections,
+    a list of IMAGES so the client can share them
+    """
+    values = message.split(" ")
+    if len(values) < 4:
+        response = '\033[31mERR INVALID_MESSAGE_FORMAT\033[0m'
+    else:
+        client_password = values[1]
+        client_port = values[2]
+        client_images = values[3]
+    
+    splitted_images = client_images.split(';')
+    images = []
+
+    for images_info in splitted_images:
+        try:
+            md5_hash, name = images_info.split(',')
+            images.append((md5_hash, name))
+        except ValueError:
+            response = '\033[31mERR INVALID_MESSAGE_FORMAT\033[0m'
+            server.sendto(response.encode(), client_address)
+            return
+    
+    hashed_pass = hashlib.sha256(client_password.encode()).hexdigest()
+    online_clients[client_address] = {'password': hashed_pass, 'port': client_port}
+    available_images[client_address] = images
+
+    num_images = len(images)
+    response = f'\033[32mOK {num_images}_REGISTERED_IMAGES\033[0m'
+    
+    for client, info in online_clients.items():
+        print(f"Client: {client}, TCP Port: {info['port']}, PASS: {info['password']}")
+    """
+    This function gets all the information from the client
+    then it saves client information in 'online_clients{}'
+    and saves client images in 'shared_images{}'
+    """
+    server.sendto(response.encode(), client_address)
 
 
 def handle_command(server, message, client_address):
     print(f"Received message from {client_address}: {message}")
-    command = message.split()
+    command = message.split(' ')
 
     if command[0] == 'REG':
-        #handle_register()
-        response = 'REG'
+        handle_register(message, client_address, server)
     elif command[0] == 'UPD':
         #handle_update()
         response = 'UPD'
@@ -22,10 +64,8 @@ def handle_command(server, message, client_address):
         #handle_end()
         response = 'END'
     else:
-        response = 'ERR INVALID_MESSAGE_FORMAT'
-    
-    print(f"Sending response to {client_address}: {response}")
-    server.sendto(response.encode(), client_address)
+        response = '\033[31mERR INVALID_MESSAGE_FORMAT\033[0m'
+        server.sendto(response.encode(), client_address)
         
 
 def main():
